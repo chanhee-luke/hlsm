dc4bb7c94222775a858a19c69125ca51d7cc4c27
song.1855 <song.1855@cse-cnc196909s.coeit.osu.edu>
2022-04-22 21:06:02 -0400

    changed init to include et data directory

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Change this to your desired directory:
-export WS_DIR=${SCRIPT_DIR}/workspace
+export WS_DIR=/local/scratch/song.1855/hlsm/workspace
 
 # Stuff that ALFRED needs
 export ALFRED_PARENT_DIR=${WS_DIR}/alfred_src
@@ -14,3 +14,6 @@ export PYTHONPATH=$PYTHONPATH:${ALFRED_PARENT_DIR}:${ALFRED_ROOT}:${ALFRED_ROOT}
 export LGP_WS_DIR="${WS_DIR}"
 export LGP_MODEL_DIR="${WS_DIR}/models"
 export LGP_DATA_DIR="${WS_DIR}/data"
+
+# TEACH specific
+export ET_DATA=/research/nfs_su_809/vln/teach-dataset
